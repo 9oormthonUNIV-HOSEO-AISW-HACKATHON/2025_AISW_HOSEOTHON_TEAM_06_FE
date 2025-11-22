341cabe83d8614d6e9c7a1686eb629dba228fe31
hyunjung <poggo201103@gmail.com>
2025-11-22 19:40:34 +0900

    add auth storage, axios file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import {Route, Routes, BrowserRouter} from 'react-router-dom'
 import './App.css'
+import AuthContext from './context/AuthContext.jsx'
 import Home from './components/home/home.jsx'
 import SignIn from './components/sign/signin.jsx'
 import SignUp from './components/sign/signup.jsx'
@@ -8,13 +9,15 @@ import SignUp from './components/sign/signup.jsx'
 function App() {
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/signIn' element={<SignIn />} />
-                <Route path='/signUp' element={<SignUp />} />
-            </Routes>
-        </BrowserRouter>
+        <AuthContext>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/signIn' element={<SignIn />} />
+                    <Route path='/signUp' element={<SignUp />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthContext>
     )
 }
 
